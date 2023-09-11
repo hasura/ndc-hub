57864b7fca351806d4d2fd51b537b5f32049d5a6
Samir Talwar <samir.talwar@hasura.io>
2023-09-11 14:28:28 +0200

    Print error messages a little more nicely in the example `main`.

--- rust-connector-sdk/bin/main.rs
+++ rust-connector-sdk/bin/main.rs
@@ -1,10 +1,19 @@
-use ndc_sdk::{connector::example::Example, default_main::default_main};
+use std::process::ExitCode;
+
+use ndc_sdk::connector::example::Example;
+use ndc_sdk::default_main::default_main;
 
 /// Run the [`Example`] connector using the [`default_main`]
 /// function, which accepts standard configuration options
 /// via the command line, configures metrics and trace
 /// collection, and starts a server.
 #[tokio::main]
-pub async fn main() {
-    default_main::<Example>().await.unwrap()
+pub async fn main() -> ExitCode {
+    match default_main::<Example>().await {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("{}", err);
+            ExitCode::FAILURE
+        }
+    }
 }
