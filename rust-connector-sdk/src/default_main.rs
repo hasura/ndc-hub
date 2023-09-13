b0d2f5a000c5c0790414628bb86caa5c8f1fb3c1
Samir Talwar <samir.talwar@hasura.io>
2023-09-13 20:35:10 +0200

    Commentary.

--- rust-connector-sdk/src/default_main.rs
+++ rust-connector-sdk/src/default_main.rs
@@ -200,18 +200,20 @@ where
     axum::Server::bind(&address)
         .serve(router.into_make_service())
         .with_graceful_shutdown(async {
+            // wait for a SIGINT, i.e. a Ctrl+C from the keyboard
             let sigint = async {
                 tokio::signal::ctrl_c()
                     .await
                     .expect("unable to install signal handler")
             };
+            // wait for a SIGTERM, i.e. a normal `kill` command
             let sigterm = async {
                 tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
                     .expect("failed to install signal handler")
                     .recv()
-                    .await;
+                    .await
             };
-
+            // block until either of the above happens
             tokio::select! {
                 _ = sigint => (),
                 _ = sigterm => (),
