bbaada311a9a8e448ca644a7d3c3f2125107707e
Gil Mizrahi <gil@gilmi.net>
2024-08-12 16:15:03 +0300

    Update update-ndc-postgres.sh registry -> registry/hasura (#230)
    https://hasurahq.slack.com/archives/C04NS5JCD8A/p1723455972067679

--- scripts/update-ndc-postgres.sh
+++ scripts/update-ndc-postgres.sh
@@ -33,18 +33,18 @@ jq \
     --arg tag "${TAG}" \
     --arg hash "${HASH}" \
     '.source_code.version += [{"tag": $tag, "hash": $hash, "is_verified": true}]' \
-    registry/"${variant}"/metadata.json \
-    > registry/"${variant}"/metadata.json2
+    registry/hasura/"${variant}"/metadata.json \
+    > registry/hasura/"${variant}"/metadata.json2
 
-mv registry/"${variant}"/metadata.json2 registry/"${variant}"/metadata.json
+mv registry/hasura/"${variant}"/metadata.json2 registry/hasura/"${variant}"/metadata.json
 
 # set latest version
 jq \
     --arg tag "${TAG}" \
     '.overview.latest_version |= $tag' \
-    registry/"${variant}"/metadata.json \
-    > registry/"${variant}"/metadata.json2
+    registry/hasura/"${variant}"/metadata.json \
+    > registry/hasura/"${variant}"/metadata.json2
 
-mv registry/"${variant}"/metadata.json2 registry/"${variant}"/metadata.json
+mv registry/hasura/"${variant}"/metadata.json2 registry/hasura/"${variant}"/metadata.json
 
 done
