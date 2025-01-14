5eb6e02eaf3756c6ddc437a6f8d4ca956af3f2e7
Vishnu Bharathi <vishnu@hasura.io>
2025-01-14 14:02:54 +0530

    more detailed error message for missing v

--- registry-automation/pkg/validate/connector_packaging.go
+++ registry-automation/pkg/validate/connector_packaging.go
@@ -2,12 +2,17 @@ package validate
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hasura/ndc-hub/registry-automation/pkg/ndchub"
 	"golang.org/x/mod/semver"
 )
 
 func ConnectorPackaging(cp *ndchub.ConnectorPackaging) error {
+	// validate version field
+	if !strings.HasPrefix(cp.Version, "v") {
+		return fmt.Errorf("version must start with 'v': but got %s", cp.Version)
+	}
 	if !semver.IsValid(cp.Version) {
 		return fmt.Errorf("invalid semantic version: %s", cp.Version)
 	}
