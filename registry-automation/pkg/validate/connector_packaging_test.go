bda40258bd658da1e6b8f98f03dfd730c84750df
Vishnu Bharathi <vishnu@hasura.io>
2025-01-15 22:54:40 +0530

    update the test

--- registry-automation/pkg/validate/connector_packaging_test.go
+++ registry-automation/pkg/validate/connector_packaging_test.go
@@ -1,6 +1,10 @@
 package validate
 
 import (
+	"crypto/sha256"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/hasura/ndc-hub/registry-automation/pkg/ndchub"
@@ -33,6 +37,73 @@ func TestConnectorPackaging(t *testing.T) {
 			}
 			if !tc.wantErr && err != nil {
 				t.Errorf("ConnectorPackaging() error = %v, wantErr %v", err, tc.wantErr)
+			}
+		})
+	}
+}
+
+func TestCheckConnectorTarball(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("dummy content"))
+	}))
+	defer server.Close()
+
+	tests := []struct {
+		name        string
+		cp          *ndchub.ConnectorPackaging
+		expectError bool
+	}{
+		{
+			name: "Valid tarball",
+			cp: &ndchub.ConnectorPackaging{
+				URI: server.URL,
+				Checksum: ndchub.Checksum{
+					Type:  "sha256",
+					Value: fmt.Sprintf("%x", sha256.Sum256([]byte("dummy content"))),
+				},
+			},
+			expectError: false,
+		},
+		{
+			name: "Invalid checksum",
+			cp: &ndchub.ConnectorPackaging{
+				URI: server.URL,
+				Checksum: ndchub.Checksum{
+					Type:  "sha256",
+					Value: "invalid_checksum",
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "Unsupported checksum type",
+			cp: &ndchub.ConnectorPackaging{
+				URI: server.URL,
+				Checksum: ndchub.Checksum{
+					Type:  "md5",
+					Value: "some_value",
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "Invalid URI",
+			cp: &ndchub.ConnectorPackaging{
+				URI: "invalid_url",
+				Checksum: ndchub.Checksum{
+					Type:  "sha256",
+					Value: "some_value",
+				},
+			},
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := checkConnectorTarball(tt.cp)
+			if (err != nil) != tt.expectError {
+				t.Errorf("checkConnectorTarball() error = %v, expectError %v", err, tt.expectError)
 			}
 		})
 	}
