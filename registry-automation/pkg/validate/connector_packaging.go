f557c1ead7bab601bf1d588a3f9ae19d6cc1ee21
Vishnu Bharathi <vishnu@hasura.io>
2025-01-15 20:28:10 +0530

    ci: add validation for connector tarball and checksum

--- registry-automation/pkg/validate/connector_packaging.go
+++ registry-automation/pkg/validate/connector_packaging.go
@@ -1,7 +1,12 @@
 package validate
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"strings"
 
 	"github.com/hasura/ndc-hub/registry-automation/pkg/ndchub"
@@ -17,5 +22,65 @@ func ConnectorPackaging(cp *ndchub.ConnectorPackaging) error {
 		return fmt.Errorf("invalid semantic version: %s", cp.Version)
 	}
 
+	// validate uri and checksum fields
+	connectorTgzFile, err := os.CreateTemp("", "connector-*.tgz")
+	if err != nil {
+		return err
+	}
+	defer connectorTgzFile.Close()
+	err = downloadFile(cp.URI, connectorTgzFile)
+	if err != nil {
+		return err
+	}
+	computeChecksum, ok := checksumFuncs[cp.Checksum.Type]
+	if !ok {
+		return fmt.Errorf("unsupported checksum type: %s", cp.Checksum.Type)
+	}
+	checksum, err := computeChecksum(connectorTgzFile)
+	if err != nil {
+		return err
+	}
+	if checksum != cp.Checksum.Value {
+		return fmt.Errorf("checksum mismatch: checksum of downloaded file: %s, but checksum in connector-packaging.json: %s", checksum, cp.Checksum.Value)
+	}
+
 	return nil
+}
+
+func downloadFile(uri string, destFile *os.File) error {
+	var err error
+
+	log.Println("starting download: ", uri)
+	resp, err := http.Get(uri)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error downloading: status code %d", resp.StatusCode)
+	}
+
+	_, err = io.Copy(destFile, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var checksumFuncs map[string]func(*os.File) (string, error) = map[string]func(*os.File) (string, error){
+	"sha256": getSHA256,
+}
+
+func getSHA256(file *os.File) (string, error) {
+	hash := sha256.New()
+
+	_, err := io.Copy(hash, file)
+	if err != nil {
+		return "", err
+	}
+
+	checksum := hash.Sum(nil)
+	return fmt.Sprintf("%x", checksum), nil
 }
