9b2569d0962e9e9987904f8ddd3fa599aaf1e188
Vishnu Bharathi <vishnu@hasura.io>
2025-01-15 23:04:47 +0530

    ci: add validation for connector tarball and checksum (#435)
    This validates all connector tarball URIs making sure the downloaded
    tarball yields a checksum that the author has mentioned in
    `connector-packaging.json` file.

--- registry-automation/pkg/validate/connector_packaging.go
+++ registry-automation/pkg/validate/connector_packaging.go
@@ -1,7 +1,11 @@
 package validate
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"hash"
+	"io"
+	"net/http"
 	"strings"
 
 	"github.com/hasura/ndc-hub/registry-automation/pkg/ndchub"
@@ -10,12 +14,68 @@ import (
 
 func ConnectorPackaging(cp *ndchub.ConnectorPackaging) error {
 	// validate version field
-	if !strings.HasPrefix(cp.Version, "v") {
-		return fmt.Errorf("version must start with 'v': but got %s", cp.Version)
+	if err := checkVersion(cp.Version); err != nil {
+		return err
 	}
-	if !semver.IsValid(cp.Version) {
-		return fmt.Errorf("invalid semantic version: %s", cp.Version)
+
+	// validate uri and checksum fields
+	if err := checkConnectorTarball(cp); err != nil {
+		return err
 	}
 
 	return nil
+}
+
+func checkVersion(version string) error {
+	if !strings.HasPrefix(version, "v") {
+		return fmt.Errorf("version must start with 'v': but got %s", version)
+	}
+	if !semver.IsValid(version) {
+		return fmt.Errorf("invalid semantic version: %s", version)
+	}
+	return nil
+}
+
+func checkConnectorTarball(cp *ndchub.ConnectorPackaging) error {
+	var checksumFuncs map[string]hash.Hash = map[string]hash.Hash{
+		"sha256": sha256.New(),
+	}
+
+	fileContents, err := downloadFile(cp.URI)
+	if err != nil {
+		return err
+	}
+
+	hashFunc, ok := checksumFuncs[cp.Checksum.Type]
+	if !ok {
+		return fmt.Errorf("unsupported checksum type: %s", cp.Checksum.Type)
+	}
+
+	_, err = io.Copy(hashFunc, fileContents)
+	if err != nil {
+		return err
+	}
+	defer fileContents.Close()
+
+	checksum := fmt.Sprintf("%x", hashFunc.Sum(nil))
+	if checksum != cp.Checksum.Value {
+		return fmt.Errorf("checksum mismatch: checksum of downloaded file: %s, but checksum in connector-packaging.json: %s", checksum, cp.Checksum.Value)
+	}
+
+	return nil
+}
+
+func downloadFile(uri string) (io.ReadCloser, error) {
+	var err error
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error downloading: status code %d", resp.StatusCode)
+	}
+
+	return resp.Body, nil
 }
