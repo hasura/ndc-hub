98c10426f0098860aa1eeb3e987e38916c437679
Karthikeyan C <karthikeyan@hasura.io>
2025-02-11 14:14:03 +0530

    validate the latest versions of all the connectors present

--- registry-automation/cmd/validate.go
+++ registry-automation/cmd/validate.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/hasura/ndc-hub/registry-automation/pkg/ndchub"
 	"github.com/hasura/ndc-hub/registry-automation/pkg/validate"
@@ -47,6 +49,14 @@ func executeValidateCmd(cmd *cobra.Command, args []string) {
 		connectorPackage *ndchub.ConnectorPackaging
 	}
 	var connectorPkgs []connectorPackaging
+
+	// Track all connectors to validate their versions
+	type connectorMetadataWithVersion struct {
+		connector     Connector
+		latestVersion string
+	}
+	var connectorsToValidate []connectorMetadataWithVersion
+
 	err = filepath.WalkDir(registryFolder, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -62,6 +72,32 @@ func executeValidateCmd(cmd *cobra.Command, args []string) {
 			}
 		}
 
+		// Check for metadata.json files
+		if filepath.Base(path) == "metadata.json" && !strings.Contains(path, "aliased_connectors") {
+			metadata, err := os.ReadFile(path)
+			var connectorMetadata ConnectorMetadata
+			err = json.Unmarshal(metadata, &connectorMetadata)
+			if err != nil {
+				return fmt.Errorf("failed to read metadata.json at %s: %v", path, err)
+			}
+
+			if err != nil {
+				return fmt.Errorf("failed to read metadata.json at %s: %v", path, err)
+			}
+
+			// Get namespace and name from path
+			connectorFolder := filepath.Dir(path)
+			namespaceFolder := filepath.Dir(connectorFolder)
+
+			connectorsToValidate = append(connectorsToValidate, connectorMetadataWithVersion{
+				connector: Connector{
+					Name:      filepath.Base(connectorFolder),
+					Namespace: filepath.Base(namespaceFolder),
+				},
+				latestVersion: connectorMetadata.Overview.LatestVersion,
+			})
+		}
+
 		return nil
 	})
 	if err != nil {
@@ -74,6 +110,7 @@ func executeValidateCmd(cmd *cobra.Command, args []string) {
 
 	fmt.Println("Validating `connector-packaging.json` contents")
 	for _, cp := range connectorPkgs {
+		println("validating connector packaging for", cp.connectorPackage.Namespace, cp.connectorPackage.Name, "with version", cp.connectorPackage.Version)
 		err := validate.ConnectorPackaging(cp.connectorPackage)
 		if err != nil {
 			fmt.Println("error validating connector packaging", cp.filePath, err)
@@ -82,8 +119,87 @@ func executeValidateCmd(cmd *cobra.Command, args []string) {
 	}
 	fmt.Println("Completed validating `connector-packaging.json` contents")
 
+	fmt.Println("Validating latest versions in metadata.json")
+	for _, cm := range connectorsToValidate {
+		println("validating latest version for", cm.connector.Namespace, cm.connector.Name, "with version", cm.latestVersion)
+		err := validateLatestVersion(cm.connector, cm.latestVersion)
+		if err != nil {
+			fmt.Printf("error validating latest version for %s/%s: %v\n",
+				cm.connector.Namespace, cm.connector.Name, err)
+			hasError = true
+		}
+	}
+	fmt.Println("Completed validating latest versions")
+
 	if hasError {
 		fmt.Println("Exiting with a non-zero error code due to the error(s) in validation")
 		os.Exit(1)
 	}
 }
+
+// func executeValidateCmd(cmd *cobra.Command, args []string) {
+// 	ndcHubGitRepoFilePath := os.Getenv("NDC_HUB_GIT_REPO_FILE_PATH")
+// 	if ndcHubGitRepoFilePath == "" {
+// 		fmt.Println("please set a value for NDC_HUB_GIT_REPO_FILE_PATH env var")
+// 		os.Exit(1)
+// 		return
+// 	}
+
+// 	registryFolder := filepath.Join(ndcHubGitRepoFilePath, "registry")
+// 	_, err := os.Stat(registryFolder)
+// 	if err != nil {
+// 		fmt.Println("error while finding the registry folder", err)
+// 		os.Exit(1)
+// 		return
+// 	}
+// 	if os.IsNotExist(err) {
+// 		fmt.Println("registry folder does not exist")
+// 		os.Exit(1)
+// 		return
+// 	}
+
+// 	type connectorPackaging struct {
+// 		filePath         string
+// 		connectorPackage *ndchub.ConnectorPackaging
+// 	}
+// 	var connectorPkgs []connectorPackaging
+// 	err = filepath.WalkDir(registryFolder, func(path string, d fs.DirEntry, err error) error {
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		if filepath.Base(path) == ndchub.ConnectorPackagingJSON {
+// 			cp, err := ndchub.GetConnectorPackaging(path)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			if cp != nil {
+// 				connectorPkgs = append(connectorPkgs, connectorPackaging{filePath: path, connectorPackage: cp})
+// 			}
+// 		}
+
+// 		return nil
+// 	})
+// 	if err != nil {
+// 		fmt.Println("error while walking the registry folder", err)
+// 		os.Exit(1)
+// 		return
+// 	}
+
+// 	hasError := false
+
+// 	fmt.Println("Validating `connector-packaging.json` contents")
+// 	for _, cp := range connectorPkgs {
+// 		err := validate.ConnectorPackaging(cp.connectorPackage)
+// 		if err != nil {
+// 			fmt.Println("error validating connector packaging", cp.filePath, err)
+// 			hasError = true
+// 		}
+// 	}
+// 	fmt.Println("Completed validating `connector-packaging.json` contents")
+
+// 	if hasError {
+// 		fmt.Println("Exiting with a non-zero error code due to the error(s) in validation")
+// 		os.Exit(1)
+// 	}
+// }
