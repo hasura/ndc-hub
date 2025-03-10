e65a9d11cfc3571333962b27d9fbdf49f3f9b7dd
m-Bilal <mohd.bilal1996@gmail.com>
2025-03-10 18:09:33 +0530

    add end to end tests for ndc-elasticsearch

---
+++ registry/hasura/elasticsearch/tests/data/init-elasticsearch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Waiting for Elasticsearch to be ready..."
+
+until curl -s -u elastic:elastic http://elasticsearch:9200/_cluster/health | grep '"status":"green"\|"status":"yellow"'; do
+    sleep 5
+done
+
+echo "Elasticsearch is ready. Adding mappings and data..."
+
+# Add customers data
+echo "Adding mapping::customers"
+sh /usr/share/elasticsearch/config/customers.sh
+
+# Add customers data
+# echo "Adding data::customers"
+# sh /usr/share/elasticsearch/data/customers.data.sh
+
+echo "Mappings and data loaded successfully!"
