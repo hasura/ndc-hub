1c20e1cd0bfd1b31cb29d5d6ed298b36a1e29c62
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2025-05-02 14:55:44 +0530

    remove double quotes from connector name

--- registry-automation/e2e-testing/utils.ts
+++ registry-automation/e2e-testing/utils.ts
@@ -164,7 +164,7 @@ export async function connector_init(
   const args: string[] = [
     "connector",
     "init",
-    `"${options.connectorName}"`,
+    `${options.connectorName}`,
     "--hub-connector",
     `"${options.hubID}"`,
     "--configure-port",
