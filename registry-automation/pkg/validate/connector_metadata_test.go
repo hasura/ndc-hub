92c425f6ec89eaf7f77b8f0b764c3369f6c2b267
Brandon Martin <brandon@codedmart.com>
2025-09-05 09:21:02 -0600

    Do not use beta/alpha for latest version check

--- registry-automation/pkg/validate/connector_metadata_test.go
+++ registry-automation/pkg/validate/connector_metadata_test.go
@@ -49,8 +49,8 @@ func TestValidateLatestVersion(t *testing.T) {
 			errContains:   "invalid semver format for version",
 		},
 		{
-			name:          "versions with pre-release tags",
-			latestVersion: "2.0.0-beta",
+			name:          "versions with pre-release tags should compare against stable version",
+			latestVersion: "1.0.0",
 			allVersions:   []string{"1.0.0", "2.0.0-alpha", "2.0.0-beta"},
 			wantErr:       false,
 		},
@@ -64,6 +64,42 @@ func TestValidateLatestVersion(t *testing.T) {
 			name:          "versions with build metadata",
 			latestVersion: "1.2.3+build.123",
 			allVersions:   []string{"1.0.0", "1.1.0+build.111", "1.2.3+build.123"},
+			wantErr:       false,
+		},
+		{
+			name:          "stable version with newer beta version should not error",
+			latestVersion: "1.2.16",
+			allVersions:   []string{"1.2.15", "1.2.16", "1.2.17-beta.1"},
+			wantErr:       false,
+		},
+		{
+			name:          "stable version with newer alpha version should not error",
+			latestVersion: "1.1.3",
+			allVersions:   []string{"1.1.2", "1.1.3", "1.1.4-alpha.1"},
+			wantErr:       false,
+		},
+		{
+			name:          "only pre-release versions should not error",
+			latestVersion: "1.0.0-beta.1",
+			allVersions:   []string{"1.0.0-alpha.1", "1.0.0-beta.1", "1.0.0-rc.1"},
+			wantErr:       false,
+		},
+		{
+			name:          "mixed stable and pre-release versions",
+			latestVersion: "2.0.0",
+			allVersions:   []string{"1.9.0", "2.0.0", "2.1.0-beta.1", "2.0.1-alpha.1"},
+			wantErr:       false,
+		},
+		{
+			name:          "postgres-promptql case: v1.2.16 with v1.2.17-beta.1",
+			latestVersion: "v1.2.16",
+			allVersions:   []string{"v1.2.15", "v1.2.16", "v1.2.17-beta.1"},
+			wantErr:       false,
+		},
+		{
+			name:          "snowflake case: v1.1.3 with v1.1.4-beta.1",
+			latestVersion: "v1.1.3",
+			allVersions:   []string{"v1.1.2", "v1.1.3", "v1.1.4-beta.1"},
 			wantErr:       false,
 		},
 	}
